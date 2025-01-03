ccce1b8df4a35687d1459f92a40ba7c957b318ee
louis.bosch <louis.bosch@agoracalyce.com>
2025-01-03 10:16:52 +0100

    si-poste

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  purge: false,
   theme: {
     extend: {
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        white: '#ffffff',
+        agora: '#b60945', // Ajout de la couleur agora
+      },
       fontFamily: {
         sans: ['Poppins', 'sans-serif'],
       },
